86eb6b3108a5295bbcafbc054d75a06e4ab494a7
andrebakker <andre_bakker@yahoo.com.br>
2015-05-06 16:39:58 -0300

    Added rests buttons (not working yet), and started a buttonGroup Controller (not working yet)

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -10,27 +10,58 @@ Vex.UI.Toolbar = function(handler){
 
 		this.buttons.wholeNote = noteValuesGroup.appendChild(this.createIcon("wholeNote", "musisync-wholeNote"));
 		this.buttons.halfNote = noteValuesGroup.appendChild(this.createIcon("halfNote", "musisync-halfNote"));
+		this.buttons.quarterNote = noteValuesGroup.appendChild(this.createIcon("quarterNote", "musisync-quarterNote"));
 		this.buttons.eightNote = noteValuesGroup.appendChild(this.createIcon("eightNote", "musisync-eightNote"));
 		this.buttons.sixteenthNote = noteValuesGroup.appendChild(this.createIcon("sixteenthNote", "musisync-sixteenthNote"));
 
 		this.handler.container.appendChild(noteValuesGroup);
+
+
+		var restValuesGroup = this.createButtonGroup("restValues");
+		this.buttonGroups.restValues = restValuesGroup;
+
+		this.buttons.wholeRest = restValuesGroup.appendChild(this.createIcon("wholeRest", "musisync-wholeRest"));
+		this.buttons.halfRest = restValuesGroup.appendChild(this.createIcon("halfRest", "musisync-halfRest"));
+		this.buttons.quarterRest = restValuesGroup.appendChild(this.createIcon("quarterRest", "musisync-quarterRest"));
+		this.buttons.eightRest = restValuesGroup.appendChild(this.createIcon("eightRest", "musisync-eightRest"));
+		this.buttons.sixteenthRest = restValuesGroup.appendChild(this.createIcon("sixteenthRest", "musisync-sixteenthRest"));
+
+		this.handler.container.appendChild(restValuesGroup);
 	}
 	if(opts.canPlay){
 		this.buttons.play = this.handler.container.appendChild(this.createIcon("play", "icomoon-play"));
 		this.buttons.stop = this.handler.container.appendChild(this.createIcon("stop", "icomoon-stop", true));
 	}
+
+
+
+	//Create a Controller that will manage which part of the toolbar will be shown
+	this.createTickableController();
 };
 
 
 Vex.UI.Toolbar.prototype.handleEvent = function(evt){
-
-	evt.preventDefault();
 	switch(evt.target.name){
 		case "play":
 			this.handler.play();
 			break;
 		case "stop":
 			this.handler.stop();
+			break;
+		case "wholeNote":
+			this.handler.updateProvisoryDuration(1);
+			break;
+		case "halfNote":
+			this.handler.updateProvisoryDuration(2);
+			break;
+		case "quarterNote":
+			this.handler.updateProvisoryDuration(4);
+			break;
+		case "eightNote":
+			this.handler.updateProvisoryDuration(8);
+			break;
+		case "sixteenthNote":
+			this.handler.updateProvisoryDuration(16);
 			break;
 	}
 };
@@ -72,3 +103,20 @@ Vex.UI.Toolbar.prototype.createButtonGroup = function(groupName){
 	return span;
 };
 
+Vex.UI.Toolbar.prototype.createTickableController = function(){
+	var tickableController = this.createButtonGroup("tickableController");
+	
+	//Create a button for every button group added.
+	var groups = Object.keys(this.buttonGroups);
+	for(var i = 0; i<groups.length; i++){
+		var group = this.buttonGroups[groups[i]];
+
+		var icon = tickableController.appendChild(this.createIcon(group.name));
+		icon.innerHTML = group.name;
+	}
+
+	this.buttonGroups.tickableController = tickableController;
+
+	//prepend the controller before anything
+	this.handler.container.insertBefore(tickableController, this.handler.container.children[0]);
+};
